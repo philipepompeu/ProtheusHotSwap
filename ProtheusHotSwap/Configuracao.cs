72df60895984e3a955d3997704c9d823b266a552
Philipe Pompeu <philipepompeu2@gmail.com>
2017-05-04 09:41:08 -0300

    Criação da classe Configuração para leitura dos dados apartir de um json

---
+++ ProtheusHotSwap/Configuracao.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProtheusHotSwap
+{
+    class Configuracao
+    {
+        public string AmbienteAtual { get; set; }
+        public string CaminhoProtheus { get; set; }
+        public string RpoBase { get; set; }
+
+        public static Configuracao ObterConfiguracao(string configFile)
+        {
+            Configuracao config;
+            if (!File.Exists(configFile))
+            {
+                config = new Configuracao()
+                {
+                    AmbienteAtual = "PRODUCAO1",
+                    CaminhoProtheus = @"C:\Protheus 11\Protheus",
+                    RpoBase = @"apo\compilador\TTTP110.RPO"
+                };
+
+                string json = JsonConvert.SerializeObject(config);
+                File.WriteAllText(configFile, json);
+            }
+            else
+            {
+                using (StreamReader file = new StreamReader(configFile))
+                {
+                    string json = file.ReadToEnd();
+                    config = JsonConvert.DeserializeObject<Configuracao>(json);
+                }
+            }
+            return config;
+        }
+    }
+}
